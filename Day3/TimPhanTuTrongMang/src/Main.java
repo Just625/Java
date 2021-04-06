eca5b40c04e25841e36281e9b2e43229c8817d92
Just625 <bloodseekerofdota@gmail.com>
2021-04-06 09:43:38 +0700

    Tim vi tri

---
+++ Day3/TimPhanTuTrongMang/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int index = -1;
+        String input;
+        String[] studentArr = {"TimberSaw", "Axe", "Tide Hunter", "Legion Commander", "Pangolier", "Underlord"};
+        Scanner src = new Scanner(System.in);
+        System.out.println("Nhap vao ten sinh vien muon tim:");
+        input = src.nextLine();
+        for(int i =0;i<studentArr.length;i++){
+            if(input.equals(studentArr[i])){
+                index = i;
+                break;
+            }
+        }
+        if(index==-1){
+            System.out.printf("Khong tim thay %s", input);
+        }else{
+            System.out.printf("%s nam o vi tri thu %d", input, index);
+        }
+
+    }
+}
