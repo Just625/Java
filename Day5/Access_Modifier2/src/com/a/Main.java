98e67706ea4b0fd60816519705f7eaa036bb0c58
Just625 <bloodseekerofdota@gmail.com>
2021-04-08 18:52:58 +0700

    Acess Modifier2

---
+++ Day5/Access_Modifier2/src/com/a/Main.java
@@ -0,0 +1,14 @@
+package com.a;
+import com.codegym.*;
+
+public class Main extends Circle{
+    public static void main(String[] args) {
+        Main main = new Main();
+        Circle circle3 = new Circle();
+        System.out.println(circle3.getRadius());
+        main.printA();
+
+    }
+
+
+}
---
+++ Day5/Access_Modifier2/src/com/codegym/Circle.java
@@ -0,0 +1,26 @@
+package com.codegym;
+
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+
+    public Circle() {
+    }
+
+    public Circle(double r) {
+        this.radius = r;
+    }
+    protected void printA(){
+        System.out.println("A");
+    }
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public double getArea() {
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+
+    protected void radius() {
+    }
+}
