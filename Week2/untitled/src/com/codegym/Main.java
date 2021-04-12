fc3321c7a5f2767815e5f47a32fcc25e87c93d3c
Just625 <bloodseekerofdota@gmail.com>
2021-04-12 16:08:39 +0700

    interface Comparator

--- Week2/untitled/src/com/codegym/Main.java
+++ Week2/untitled/src/com/codegym/Main.java
@@ -1,6 +1,7 @@
 package com.codegym;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.Collector;
 
 public class Main {
@@ -12,8 +13,20 @@ public class Main {
             circles[i] = new Circle(Math.floor(Math.random()*20));
             System.out.println(circles[i]);
         }
-        CircleComparator circleComparator = new CircleComparator();
-        Arrays.sort(circles, circleComparator);
+//        CircleComparator circleComparator = new CircleComparator();
+//        Arrays.sort(circles, circleComparator);
+        Arrays.sort(circles, new Comparator<Circle>() {
+            @Override
+            public int compare(Circle o1, Circle o2) {
+                if(o1.getRadius()>o2.getRadius()){
+                    return 1;
+                } else if (o1.getRadius()<o2.getRadius()) {
+                    return -1;
+                }else{
+                    return 0;
+                }
+            }
+        });
         System.out.println("Sau khi sort");
         for(int j = 0; j < circles.length;j++){
             System.out.println(circles[j]);
