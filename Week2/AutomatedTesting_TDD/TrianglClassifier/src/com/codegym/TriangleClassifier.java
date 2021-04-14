7b7b39a466c3d4b2ae38438982e7b2304cb09467
Just625 <bloodseekerofdota@gmail.com>
2021-04-14 14:49:39 +0700

    refactor class

--- Week2/AutomatedTesting_TDD/TrianglClassifier/src/com/codegym/TriangleClassifier.java
+++ Week2/AutomatedTesting_TDD/TrianglClassifier/src/com/codegym/TriangleClassifier.java
@@ -1,17 +1,25 @@
 package com.codegym;
 
 public class TriangleClassifier {
+
+    public static final String TAM_GIAC_THUONG = "tam giac thuong";
+    public static final String TAM_GIAC_CAN = "tam giac can";
+    public static final String TAM_GIAC_DEU = "tam giac deu";
+
     public String classifierTriangle(int firstNumb, int secondNumb, int thirdNumb) {
         String result = "khong phai la tam giac";
-        if ((firstNumb + secondNumb > thirdNumb)
+        boolean isTriangle = (firstNumb + secondNumb > thirdNumb)
                 && (secondNumb + thirdNumb > firstNumb)
-                && (firstNumb + thirdNumb > secondNumb)) {
-            result = "tam giac thuong";
-            if (firstNumb == secondNumb || secondNumb == thirdNumb || thirdNumb == firstNumb) {
-                result = "tam giac can";
+                && (firstNumb + thirdNumb > secondNumb);
+        if (isTriangle) {
+            result = TAM_GIAC_THUONG;
+            boolean isTamGiacCan = firstNumb == secondNumb || secondNumb == thirdNumb || thirdNumb == firstNumb;
+            if (isTamGiacCan) {
+                result = TAM_GIAC_CAN;
             }
-            if(firstNumb == secondNumb && secondNumb == thirdNumb){
-                result = "tam giac deu";
+            boolean isTamGiacDeu = firstNumb == secondNumb && secondNumb == thirdNumb;
+            if(isTamGiacDeu){
+                result = TAM_GIAC_DEU;
             }
         }
         return result;
