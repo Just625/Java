7155a7b9c76790e3cd9d1b58e6d1d19c11fcdcd0
Just625 <bloodseekerofdota@gmail.com>
2021-04-14 14:52:02 +0700

    final refactor

--- Week2/AutomatedTesting_TDD/TrianglClassifier/src/com/codegym/TriangleClassifier.java
+++ Week2/AutomatedTesting_TDD/TrianglClassifier/src/com/codegym/TriangleClassifier.java
@@ -5,9 +5,10 @@ public class TriangleClassifier {
     public static final String TAM_GIAC_THUONG = "tam giac thuong";
     public static final String TAM_GIAC_CAN = "tam giac can";
     public static final String TAM_GIAC_DEU = "tam giac deu";
+    public static final String NOT_A_TRIANGLE = "khong phai la tam giac";
 
     public String classifierTriangle(int firstNumb, int secondNumb, int thirdNumb) {
-        String result = "khong phai la tam giac";
+        String result = NOT_A_TRIANGLE;
         boolean isTriangle = (firstNumb + secondNumb > thirdNumb)
                 && (secondNumb + thirdNumb > firstNumb)
                 && (firstNumb + thirdNumb > secondNumb);
