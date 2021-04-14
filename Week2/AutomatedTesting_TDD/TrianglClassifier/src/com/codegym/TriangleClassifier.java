251790a9cbb4ca61fce07eba1a1856312ece51ef
Just625 <bloodseekerofdota@gmail.com>
2021-04-14 14:47:54 +0700

    refactor

--- Week2/AutomatedTesting_TDD/TrianglClassifier/src/com/codegym/TriangleClassifier.java
+++ Week2/AutomatedTesting_TDD/TrianglClassifier/src/com/codegym/TriangleClassifier.java
@@ -2,11 +2,17 @@ package com.codegym;
 
 public class TriangleClassifier {
     public String classifierTriangle(int firstNumb, int secondNumb, int thirdNumb) {
-        String result ="khong phai la tam giac";
+        String result = "khong phai la tam giac";
         if ((firstNumb + secondNumb > thirdNumb)
                 && (secondNumb + thirdNumb > firstNumb)
                 && (firstNumb + thirdNumb > secondNumb)) {
             result = "tam giac thuong";
+            if (firstNumb == secondNumb || secondNumb == thirdNumb || thirdNumb == firstNumb) {
+                result = "tam giac can";
+            }
+            if(firstNumb == secondNumb && secondNumb == thirdNumb){
+                result = "tam giac deu";
+            }
         }
         return result;
     }
--- Week2/AutomatedTesting_TDD/TrianglClassifier/test/TriangleClassifierTest.java
+++ Week2/AutomatedTesting_TDD/TrianglClassifier/test/TriangleClassifierTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TriangleClassifierTest {
+    public static final String TAM_GIAC_DEU = "tam giac deu";
+    public static final String TAM_GIAC_CAN = "tam giac can";
+    public static final String TAM_GIAC_THUONG = "tam giac thuong";
+    public static final String KHONG_PHAI_LA_TAM_GIAC = "khong phai la tam giac";
     TriangleClassifier triangleClassifier = new TriangleClassifier();
 
     @Test
@@ -13,7 +17,7 @@ public class TriangleClassifierTest {
         int firstNumb = 2;
         int secondNumb = 2;
         int thirdNumb = 2;
-        String expected = "tam giac thuong";
+        String expected = TAM_GIAC_DEU;
         String actual = triangleClassifier.classifierTriangle(firstNumb, secondNumb, thirdNumb);
         assertEquals(expected, actual);
     }
@@ -24,7 +28,51 @@ public class TriangleClassifierTest {
         int firstNumb = 2;
         int secondNumb = 2;
         int thirdNumb = 3;
-        String expected = "tam giac thuong";
+        String expected = TAM_GIAC_CAN;
+        String actual = triangleClassifier.classifierTriangle(firstNumb, secondNumb, thirdNumb);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("3,4,5")
+    void TriangleClassifierTest2() {
+        int firstNumb = 3;
+        int secondNumb = 4;
+        int thirdNumb = 5;
+        String expected = TAM_GIAC_THUONG;
+        String actual = triangleClassifier.classifierTriangle(firstNumb, secondNumb, thirdNumb);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("8,2,3")
+    void TriangleClassifierTest3() {
+        int firstNumb = 8;
+        int secondNumb = 2;
+        int thirdNumb = 3;
+        String expected = KHONG_PHAI_LA_TAM_GIAC;
+        String actual = triangleClassifier.classifierTriangle(firstNumb, secondNumb, thirdNumb);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("-1,2,1")
+    void TriangleClassifierTest4() {
+        int firstNumb = -1;
+        int secondNumb = 2;
+        int thirdNumb = 1;
+        String expected =KHONG_PHAI_LA_TAM_GIAC;
+        String actual = triangleClassifier.classifierTriangle(firstNumb, secondNumb, thirdNumb);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("0,1,1")
+    void TriangleClassifierTest5() {
+        int firstNumb = 0;
+        int secondNumb = 1;
+        int thirdNumb = 1;
+        String expected = KHONG_PHAI_LA_TAM_GIAC;
         String actual = triangleClassifier.classifierTriangle(firstNumb, secondNumb, thirdNumb);
         assertEquals(expected, actual);
     }
