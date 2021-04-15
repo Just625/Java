24093915257d442d9a85cb7c5aaff02a2e1c91d1
Just625 <bloodseekerofdota@gmail.com>
2021-04-15 09:18:17 +0700

    Them menu

--- Week2/AutomatedTesting_TDD/BT14.4(2)/src/com/codegym/Main.java
+++ Week2/AutomatedTesting_TDD/BT14.4(2)/src/com/codegym/Main.java
@@ -5,22 +5,40 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         Scanner src = new Scanner(System.in);
         System.out.println("Nhap vao so ho su dung dien:");
         int n = src.nextInt();
         Receipt[] receipts = new Receipt[n];
         ReceiptManagement receiptManagement = new ReceiptManagement(receipts);
-        receiptManagement.initReceipt();
-        receiptManagement.showAllReceipt();
-        receiptManagement.calculateMoneyFromCode();
+        int choice;
+        do {
+            showMenu();
+            System.out.println("enter your choice:");
+            choice = src.nextInt();
+            if (choice < 0 || choice > 3) {
+                System.out.println("Lua chon khong hop le");
+            }
+            switch (choice) {
+                case 1:
+                    receiptManagement.initReceipt();
+                    break;
+                case 2:
+                    receiptManagement.showAllReceipt();
+                    break;
+                case 3:
+                    receiptManagement.calculateMoneyFromCode();
+                    break;
+            }
+        } while (choice != 0 || choice > 3);
     }
 
-
-
-
-
-
+    private static void showMenu() {
+        System.out.println("1. Nhap du lieu ban dau cho receipt");
+        System.out.println("2. Hien thi thong tin ");
+        System.out.println("3. Tinh tien cho customer");
+        System.out.println("0. Thoat chuong trinh");
+    }
 
 
 }
