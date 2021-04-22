fbb171e760d3f5832139ffb9289e5b93a264557d
Just625 <bloodseekerofdota@gmail.com>
2021-04-22 14:54:52 +0700

    Insertion sort

---
+++ Week2/SortingAlgorithm/CaiDatInsertionSort/src/com/codegym/Main.java
@@ -0,0 +1,37 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int[] list = {1, 9, 6, -8, 0, 4};
+        display(list);
+        insertionSort(list);
+        display(list);
+    }
+
+    public static void display(int[] list) {
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void insertionSort(int[] list) {
+        for (int i = 1; i < list.length; i++) {
+            int index = i;
+            int temp = list[index];
+            while (index > 0) {
+                if (temp < list[index - 1]) {
+                    list[index] = list[index - 1];
+                } else {
+                    break;
+                }
+                index--;
+            }
+            if (index != i) {
+                list[index] = temp;
+            }
+        }
+    }
+}
