caa41e234e788d16d41eaa84388defd6f4c964cb
Just625 <bloodseekerofdota@gmail.com>
2021-04-26 08:52:29 +0700

    Doc file .csv

---
+++ Week3/IO_TextFile/DocFileCSV/src/com/codegym/Reader.java
@@ -0,0 +1,27 @@
+package com.codegym;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Reader {
+    public void readFile(String filePath) {
+        File file = new File(filePath);
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line;
+            System.out.printf("%-2s %-4s %s\n", "ID", "Code", "Name");
+            reader.readLine();
+            while ((line = reader.readLine()) != null) {
+                String[] arr = line.split(",");
+                System.out.printf("%-2s %-4s %s\n",arr[0], arr[1], arr[2]);
+            }
+            reader.close();
+            System.out.println("The end");
+        } catch (IOException e) {
+            System.err.println("File not found");
+        }
+    }
+}
