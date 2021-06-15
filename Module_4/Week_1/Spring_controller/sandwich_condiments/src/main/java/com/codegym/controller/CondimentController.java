0010ec50d4c54cd607b7aef840e4ca18af6adcfd
Just625 <bloodseekerofdota@gmail.com>
2021-06-15 23:03:03 +0700

    [Bài tập] Ứng dụng hiển thị gia vị với Sandwich

--- Module_4/Week_1/Spring_controller/sandwich_condiments/src/main/java/com/codegym/controller/CondimentController.java
+++ Module_4/Week_1/Spring_controller/sandwich_condiments/src/main/java/com/codegym/controller/CondimentController.java
@@ -1,2 +1,24 @@
-package com.codegym.controller;public class CondimentController {
+package com.codegym.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class CondimentController {
+    @RequestMapping("/")
+    public String showForm(){
+        return "index";
+    }
+    @RequestMapping("/save")
+    public ModelAndView save(@RequestParam(value = "condiment",required = false) String[] condiments){
+        ModelAndView modelAndView = new ModelAndView("list");
+        modelAndView.addObject("condiments", condiments);
+        if(condiments==null){
+            modelAndView.addObject("message","Nothing was picked");
+        }
+        return modelAndView;
+    }
 }
--- Module_4/Week_1/Spring_controller/sandwich_condiments/src/main/webapp/WEB-INF/views/list.jsp
+++ Module_4/Week_1/Spring_controller/sandwich_condiments/src/main/webapp/WEB-INF/views/list.jsp
@@ -6,11 +6,19 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<head>
+    <title>Condiment list </title>
+</head>
+<body>
+<h1>Condiments for sandwich</h1>
+<p>${message}</p>
+<a href="/">Back to picking condiment</a>
+<ol>
+  <c:forEach items="${condiments}" var="condiment">
+    <li>${condiment}</li>
+  </c:forEach>
+</ol>
+</body>
 </html>
