4ab94e28d5ffd05c4800ff0ef142e3e873d7def7
Just625 <bloodseekerofdota@gmail.com>
2021-07-02 17:33:30 +0700

    [Bài tập] Nâng cấp ứng dụng Bức ảnh của ngày

---
+++ Module_4/Week2/Spring-data-repository/potd/src/main/java/com/codegym/service/ICommentService.java
@@ -0,0 +1,14 @@
+package com.codegym.service;
+
+import com.codegym.model.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface ICommentService {
+    Iterable<Comment> findAll();
+    void save(Comment comment);
+    Optional<Comment> findOne(Long id);
+    Page<Comment> findByDates(int dates, Pageable pageable);
+}
